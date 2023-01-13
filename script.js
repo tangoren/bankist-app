17321bceabffdd5ab72f554e1cbb39a1b994d5b9
Osman Tangoren <otangoren@gmail.com>
2023-01-13 16:56:09 +0300

    account delete feature complete with array findIndex method

--- script.js
+++ script.js
@@ -205,6 +205,30 @@ btnTransfer.addEventListener('click', function (e) {
 });
 // transfer money feature
 
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+
+    accounts.splice(index, 1);
+
+    // Update UI
+    containerApp.style.opacity = 0;
+  }
+
+  inputCloseUsername.value = inputClosePin.value = '';
+
+  inputCloseUsername.blur();
+  inputClosePin.blur();
+});
+// account delete feature
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
