9dc49017d25db170748356380a394ae0740c9fa2
Osman Tangoren <otangoren@gmail.com>
2023-01-19 10:47:28 +0300

    math and remainder examples complete

--- script.js
+++ script.js
@@ -385,6 +385,40 @@ btnSort.addEventListener('click', function (e) {
 // console.log((2.345).toFixed(2));
 // console.log(+(2.345).toFixed(2));
 
+// // The Remainder Operator
+// console.log(5 % 2);
+// console.log(5 / 2); // 5 = 2 * 2 + 1
+
+// console.log(8 % 3);
+// console.log(8 / 3); // 8 = 2 * 3 + 2
+
+// console.log(6 % 2);
+// console.log(6 / 2);
+
+// console.log(7 % 2);
+// console.log(7 / 2);
+
+// const isEven = n => n % 2 === 0;
+
+// console.log(isEven(8));
+// console.log(isEven(23));
+// console.log(isEven(514));
+
+// // Use for every nth element
+// labelBalance.addEventListener('click', function () {
+//   [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+//     // 0, 2, 4, 6
+//     if (i % 2 === 0) {
+//       row.style.backgroundColor = 'red';
+//     }
+
+//     // 0, 3, 6, 9
+//     if (i % 3 === 0) {
+//       row.style.backgroundColor = 'blue';
+//     }
+//   });
+// });
+
 // 🎗️ WORKING WITH ARRAYS
 // // Simple Array Methods
 // let arr = ['a', 'b', 'c', 'd', 'e'];
