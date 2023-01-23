149b84a4b013b18913950cbef59ed0854c8ca54a
Osman Tangoren <otangoren@gmail.com>
2023-01-23 19:42:01 +0300

    settimeout and setinterval examples complete

--- script.js
+++ script.js
@@ -351,14 +351,16 @@ btnLoan.addEventListener('click', function (e) {
   const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-    // Add movement
-    currentAccount.movements.push(amount);
+    setTimeout(function () {
+      // Add movement
+      currentAccount.movements.push(amount);
 
-    // Add loan date
-    currentAccount.movementsDates.push(new Date().toISOString());
+      // Add loan date
+      currentAccount.movementsDates.push(new Date().toISOString());
 
-    // Update UI
-    updateUI(currentAccount);
+      // Update UI
+      updateUI(currentAccount);
+    }, 2500);
   }
 
   inputLoanAmount.value = '';
@@ -629,6 +631,36 @@ btnSort.addEventListener('click', function (e) {
 //   navigator.language,
 //   new Intl.NumberFormat(navigator.language, options).format(num)
 // );
+
+///////////////////////////////////////
+// // Timers
+
+// // setTimeout
+// const ingredients = ['olives', 'spinach'];
+// const pizzaTimer = setTimeout(
+//   (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2} 🍕`),
+//   3000,
+//   ...ingredients
+// );
+
+// console.log('Waiting...');
+
+// if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
+
+// // setInterval
+// setInterval(function () {
+//   const now = new Date();
+//   console.log(now);
+// }, 1000);
+
+// setInterval(() => {
+//   const now = new Date();
+//   const h = now.getHours();
+//   const m = now.getMinutes();
+//   const s = now.getSeconds();
+
+//   console.log(`${h}:${m}:${s}`);
+// }, 1000);
 
 // 🎗️ WORKING WITH ARRAYS
 // // Simple Array Methods
